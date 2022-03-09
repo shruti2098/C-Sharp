0b5c8c21e46f3458f3d625a3d1e24f0597a5d86d
shruti2098 <shrutiiadam2098@gmail.com>
2022-03-09 17:24:36 +0530

    files added

---
+++ GenericClass.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+class GenericClass
+{
+    //public static void Main()
+    //{
+    #region stack
+    //Stack<string> numbers = new Stack<string>();
+    //numbers.Push("one");
+    //numbers.Push("two");
+    //numbers.Push("three");
+    //numbers.Push("four");
+    //numbers.Push("five");
+
+    //foreach (string number in numbers)
+    //{
+    //    Console.WriteLine(number);
+    //}
+
+    //Console.WriteLine("\nPopping '{0}'", numbers.Pop());
+    //Console.WriteLine("Peek at next item to destack: {0}",
+    //  numbers.Peek());
+    ////Console.WriteLine("Popping '{0}'", numbers.Pop());
+    //Console.WriteLine("\nstack2.Contains(\"four\") = {0}",
+    //    numbers.Contains("four"));
+
+    //Console.WriteLine("\nnumbers.Count = {0}", numbers.Count);
+    #endregion
+    #region queue
+    //Queue<string> numbers = new Queue<string>();
+    //numbers.Enqueue("one");
+    //numbers.Enqueue("two");
+    //numbers.Enqueue("three");
+    //numbers.Enqueue("four");
+    //numbers.Enqueue("five");
+
+    //// A queue can be enumerated without disturbing its contents.
+    //foreach (string number in numbers)
+    //{
+    //    Console.WriteLine(number);
+    //}
+
+    //Console.WriteLine("\nDequeuing '{0}'", numbers.Dequeue());
+    //Console.WriteLine("Peek at next item to dequeue: {0}",
+    //    numbers.Peek());
+    //Console.WriteLine("Dequeuing '{0}'", numbers.Dequeue());
+    #endregion
+    #region dictionary
+    //            Dictionary<int, string> My_dict1 = new Dictionary<int, string>();
+    //            My_dict1.Add(1123, "Aishwarya");
+    //            My_dict1.Add(1124, "Pooja");
+    //            My_dict1.Add(1125, "Shruti");
+
+    //            foreach (KeyValuePair<int, string> ele1 in My_dict1)
+    //            {
+    //                Console.WriteLine("{0} and {1}",
+    //                            ele1.Key, ele1.Value);
+    //            }
+    //            Console.WriteLine();
+    //            Dictionary<string, string> My_dict2 =
+    //                    new Dictionary<string, string>(){
+    //                                {"a.1", "Dog"},
+    //                                {"a.2", "Cat"},
+    //                                {"a.3", "Pig"} };
+
+    //            foreach (KeyValuePair<string, string> ele2 in My_dict2)
+    //            {
+    //                Console.WriteLine("{0} and {1}", ele2.Key, ele2.Value);
+    //            }
+
+
+       #endregion
+
+
+    //}
+}
+
---
+++ HashTable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+    class HashTable
+    {
+		public static void Main()
+		{
+			Hashtable numberNames = new Hashtable();
+			numberNames.Add(1, "One"); 
+			numberNames.Add(2, "Two");
+			numberNames.Add(3, "Three");
+			foreach (DictionaryEntry kvp in numberNames)
+			Console.WriteLine("Key: {0}, Value: {1}", kvp.Key, kvp.Value);
+			var cities = new Hashtable(){
+			{"UK", "London, Manchester, Birmingham"},
+			{"USA", "Chicago, New York, Washington"},
+			{"India", "Mumbai, New Delhi, Pune"}
+		};
+			foreach (DictionaryEntry kvp in cities)
+				Console.WriteLine("Key: {0}, Value: {1}", kvp.Key, kvp.Value);
+		}
+	}
+}
